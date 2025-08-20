1f644c20b9a1a7faac5bdca9b40cd679896bbe47
Prabsshrestha <prabs.shrestha@gmail.com>
2025-08-20 17:11:50 +0545

    changes

--- Prabesh/Dtos/MovieDto.cs
+++ Prabesh/Dtos/MovieDto.cs
@@ -9,22 +9,17 @@ namespace Prabesh.Dtos
     public class MovieDto
     {
         public int Id { get; set; }
-
         [Required]
         [StringLength(255)]
         public string Name { get; set; }
-
-
         [Required]
         public byte GenreId { get; set; }
         public GenreDto Genre { get; set; }
         public DateTime DateAdded { get; set; }
-
         public DateTime ReleaseDate { get; set; }
-
         [Range(1, 100)]
         public byte NumberInStock { get; set; }
-
+        public byte NumberAvailable { get; set; }
         public int Price { get; set; }
     }
 
